cbf0f5c058fb8d304d9031e694c34ce9bfe14c1c
starduda <tookawwa@gmail.com>
2024-03-19 15:08:30 -0300

    java-script

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// conversão implíicita
+const numero = 456;
+const numeroString = "456";
+
+//Number()
+//String()
+console.log(numero + Number(numeroString))
+
+//conversão explícita
