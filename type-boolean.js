24cf9a9585ec7655f959750427e69f42f90b7aff
starduda <tookawwa@gmail.com>
2024-03-19 14:27:55 -0300

    script java

---
+++ type-boolean.js
@@ -0,0 +1,14 @@
+// boolean
+//true -> verdadeiro
+//false -> false
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const cadastroAtivo = true;
+
+//console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
